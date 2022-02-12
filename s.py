19833978fa59e87df4f30efe71bc78ed377ee87f
kavya <kavyatina2@gmail.com>
2022-02-12 20:47:00 +0530

    My project

---
+++ s.py
@@ -0,0 +1,51 @@
+questions = ["Which is the largest country in the world?",
+             "How many continents are there in the world?",
+             "Which one of these four birds has the longest beak and feet?",
+             "What is the national currency of the United States of America (USA)?",
+             "Guido van Rossum in 1991 designed which language?",
+             "Cow is the national animal of which of the following country?",
+             "Which one is the first fully supported 64-bit operating system?",
+             "How many years are there in one Millenium?",
+             "Who is the first woman to win a nobel prize?",
+             "Which team has won most number of IPL matches?",
+             "Which city will host Olympics 2024?",
+             "Which continent has the highest number of countries?",
+             "Which dog has the best sense of smell?",
+             "ipad is manufactured by?",
+             "Who founded Microsoft?"]
+
+first_option = ["India", "8",
+                "Heron", "Euro",
+                "Javascript", "Nepal",
+                "Windows 7", "100 years", "Nelly Sachs", "KKR",
+                "Sydney", "Europe",
+                "Coonhound", "Google", "Monty Ritz"]
+
+second_option = ["USA", "7",
+                 "Parrot", "Peso ",
+                 "Python", "Sri Lanka",
+                 "Linux", "1000 years", "Jane Addams", "CSK",
+                 "Beijing", "Asia",
+                 "Pointer",
+                 "Microsoft", "Danis Lio"]
+
+third_option = ["China", "5",
+                "Crow", "Dollar",
+                "Java", "China",
+                "Mac", "500 years", "Marie Curie", "MI",
+                "New York", "Africa",
+                "Beagle",
+                "Amazon", "Bill Gates"]
+
+fourth_option = ["Russia", "6",
+                 "Pigeon", "Yen",
+                 "C++", "Israel",
+                 "Windows XP", "50 years", "Pearl Buck", "RCB",
+                 "Paris","antarctica",
+                 "Bloodhound", "Apple",
+                 "Jeff Bezos"]
+
+correct_answers = ["Russia", "7", "Heron", "Dollar", "Python", "Nepal",
+                   "Linux", "Bloodhound", "Marie Curie", "MI", "Paris", "Africa", "1000 years", "Apple",
+                   "Bill Gates"]
+
